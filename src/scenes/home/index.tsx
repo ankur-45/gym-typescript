5a1bfd8b176121d4e48c162991bddc5232d7cdff
ankur-45 <ankur070593@gmail.com>
2023-10-02 12:09:58 +0530

    removed redbull

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -3,7 +3,6 @@ import { SelectedPage } from '@/shared/types';
 import ActionButton from '@/shared/ActionButton';
 import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png';
-import SponsorRedbull from '@/assets/SponsorRedbull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -82,7 +81,6 @@ const Home = ({ setSelectedPage }: Props) => {
                 <div className='h-[150px] w-full bg-primary-100 py-10'>
                     <div className='mx-auto w-5/6'>
                         <div className='flex w-3/5 items-center justify-between gap-8'>
-                            <img alt="redbull-sponsor" src={SponsorRedbull} />
                             <img alt="forbes-sponsor" src={SponsorForbes} />
                             <img alt="fortune-sponsor" src={SponsorFortune} />
                         </div>
